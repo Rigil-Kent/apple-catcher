5a581d8584c97e68bfa16e99dfa3bd58f78aad61
Bryan Bailey <bcbailey84@gmail.com>
2018-02-04 13:08:43 -0500

    [Code] AppleTree
    created necessary variables

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+
 
 public class AppleTree : MonoBehaviour {
+	[Header("Set in Inspector")]
+
+	// Prefab for instantiating apples
+	public GameObject applePrefab;
+
+	// Speed at which the AppleTree moves
+	public float speed = 1f;
+
+	// Distance where the AppleTree turns around
+	public float LeftAndRightEdge = 10f;
+
+	// Chance that the AppleTree will change directions
+	public float chanceToChangeDirections = 0.1f;
+
+	// Rate at which Apples will be instantiated
+	public float secondsBetweenAppleDrops = 1f;
+
+
 
 	// Use this for initialization
 	void Start () {
-	
+	    // Droping apples every second
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    // Basic movement
+
+		// Changing Direction
 	}
 }
